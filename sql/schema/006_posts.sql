c5d1754577ea524eb5954dbf3c801e9d761c27ec
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-04-08 18:02:33 +0530

    Add Post model, SQL queries, and schema for managing posts

---
+++ sql/schema/006_posts.sql
@@ -0,0 +1,15 @@
+-- +goose up
+CREATE TABLE posts (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT,
+    published_at TIMESTAMPTZ NOT NULL,
+    url TEXT NOT NULL UNIQUE,
+    feed_id UUID NOT NULL,
+    FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+);
+
+-- +goose down
+DROP TABLE posts;
